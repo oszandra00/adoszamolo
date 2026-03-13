5c7c94343c4989d54d3f14ad5358d8a4d46abfa9
oszandra00 <olahalexandra97@gmail.com>
2026-03-14 00:58:04 +0100

    Add files via upload

---
+++ adoszamolo-deploy/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+export default function Home() {
+  return (
+    <main>
+      <iframe
+        src="/app.html"
+        style={{ width: '100%', height: '100vh', border: 'none' }}
+        title="AdóSzámoló – Építményadó bejelentés"
+      />
+    </main>
+  )
+}
